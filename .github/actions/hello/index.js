1b168576676d728d26f915a5e13cee215938781e
Simon Berscheid <simon.berscheid@adesso.de>
2022-10-17 00:37:35 +0200

    some more core functions

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -3,13 +3,23 @@ const github = require('@actions/github')
 
 try {
     // throw(new Error("some error message"));
+
+    core.debug('Debug message')
+    core.warning('Warning message')
+    core.error('Error message')
+
     const name = core.getInput('who-to-greet');
+    core.setSecret(name);
     console.log(`Hello Mister ${name}`);
 
     const time = new Date();
     core.setOutput("time", time.toTimeString());
 
+    core.startGroup("Github Object Output");
     console.log(JSON.stringify(github, null, '\t'));
+    core.endGroup();
+
+    core.exportVariable("HELLO", "hello");
 } catch (error) {
     console.log(error.message)
     core.setFailed(error.message)
