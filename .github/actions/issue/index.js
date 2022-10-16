20adf4124bc36f01c885b2468c60f11d57a143a7
Simon Berscheid <simon.berscheid@adesso.de>
2022-10-17 01:15:16 +0200

    async and try to fix

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -1,22 +1,25 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
-try {
-    const token = core.getInput('token')
-    const title = core.getInput('title')
-    const body = core.getInput('body')
-    const assignees = core.getInput('assignees')
+async function createIssue() {
+    try {
+        const token = core.getInput('token')
+        const title = core.getInput('title')
+        const body = core.getInput('body')
+        const assignees = core.getInput('assignees')
 
-    const octokit = new github.getOctokit(token);
+        const octokit = new github.getOctokit(token);
 
-    const response = octokit.rest.issues.create({
-        ...github.context.repo,
-        title,
-        body,
-        assignees: assignees ? assignees.split('\n') : undefined
-    });
+        const response = await octokit.rest.issues.create({
+            ...github.context.repo,
+            title,
+            body
+        });
 
-    core.setOutput('issue', JSON.stringify(response.data));
-} catch (error) {
-    core.setFailed(error.message)
+        core.setOutput('issue', JSON.stringify(response.data));
+    } catch (error) {
+        core.setFailed(error.message)
+    }
 }
+
+createIssue();
